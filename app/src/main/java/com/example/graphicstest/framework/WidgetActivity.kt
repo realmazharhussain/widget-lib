29dc1e48a54ebdb4f89190636b6c57d6bd2f724a
Mazhar Hussain <mazhar.hussain@texense.io>
2025-07-24 17:01:51 +0500

    Working background Color

--- app/src/main/java/com/example/graphicstest/framework/WidgetActivity.kt
+++ app/src/main/java/com/example/graphicstest/framework/WidgetActivity.kt
@@ -1,7 +1,11 @@
 package com.example.graphicstest.framework
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.graphicstest.R
 
 abstract class WidgetActivity : AppCompatActivity() {
     lateinit var content: TopLevel; private set
@@ -9,12 +13,24 @@ abstract class WidgetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_widget)
+        val view = findViewById<WidgetView>(R.id.widget_view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         val contentId = savedInstanceState?.getString("content_id")
         content = when (contentId) {
             null -> content()
             else -> topLevels[contentId] ?: throw IllegalStateException("A top level with id '$contentId' not found")
         }
         content.setActivity(activity = this)
+        view.content = content
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
--- app/src/main/java/com/example/graphicstest/framework/WidgetView.kt
+++ app/src/main/java/com/example/graphicstest/framework/WidgetView.kt
@@ -2,7 +2,9 @@ package com.example.graphicstest.framework
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.PixelFormat
 import android.util.AttributeSet
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class WidgetView @JvmOverloads constructor(
@@ -10,8 +12,36 @@ class WidgetView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0,
-) : SurfaceView(context, attrs, defStyleAttr, defStyleRes) {
+) : SurfaceView(context, attrs, defStyleAttr, defStyleRes), SurfaceHolder.Callback {
+
+    var content: TopLevel? = null
+
+    init {
+        setZOrderOnTop(true)
+        holder.setFormat(PixelFormat.RGBA_8888)
+        holder.addCallback(this)
+    }
 
     override fun onDraw(canvas: Canvas) {
+        val content = content ?: return
+        canvas.drawColor(content.backgroundColor)
     }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        triggerDraw()
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        triggerDraw()
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+    }
+
+    private fun triggerDraw() {
+        val canvas = holder.lockHardwareCanvas()
+        draw(canvas)
+        holder.unlockCanvasAndPost(canvas)
+    }
+
 }
