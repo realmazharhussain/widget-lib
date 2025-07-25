21d7f055d8f4fc0257b90fe42daa6e001b9b05fd
Mazhar Hussain <mazhar.hussain@texense.io>
2025-07-25 15:12:23 +0500

    Random Background

--- app/src/main/java/dev/mazharhussain/widget_lib/MainActivity.kt
+++ app/src/main/java/dev/mazharhussain/widget_lib/MainActivity.kt
@@ -3,10 +3,11 @@ package dev.mazharhussain.widget_lib
 import android.graphics.Color
 import dev.mazharhussain.widget_lib.framework.TopLevel
 import dev.mazharhussain.widget_lib.framework.WidgetActivity
+import kotlin.random.Random
 
 
 class MainActivity : WidgetActivity() {
     override fun content() = TopLevel(id = "main").apply {
-        backgroundColor = Color.WHITE
+        backgroundColor = Color.rgb(Random.nextInt(), Random.nextInt(), Random.nextInt())
     }
 }
