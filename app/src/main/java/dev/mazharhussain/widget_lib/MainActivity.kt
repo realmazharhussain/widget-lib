99162d94ff4983e1a1975465b9e5abc85701809b
Mazhar Hussain <mazhar.hussain@texense.io>
2025-07-25 14:42:33 +0500

    Change Background

--- app/src/main/java/dev/mazharhussain/widget_lib/MainActivity.kt
+++ app/src/main/java/dev/mazharhussain/widget_lib/MainActivity.kt
@@ -1,9 +1,12 @@
 package dev.mazharhussain.widget_lib
 
+import android.graphics.Color
 import dev.mazharhussain.widget_lib.framework.TopLevel
 import dev.mazharhussain.widget_lib.framework.WidgetActivity
 
 
 class MainActivity : WidgetActivity() {
-    override fun content() = TopLevel(id = "main")
+    override fun content() = TopLevel(id = "main").apply {
+        backgroundColor = Color.WHITE
+    }
 }
